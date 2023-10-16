2934631c271bd43a1ca51cb477a2484a42e84bce
ClearSky <ggogono2@gmail.com>
2023-10-16 12:52:39 +0900

    주석

--- mail.py
+++ mail.py
@@ -6,7 +6,18 @@ import re  # Regular Expression을 활용하기 위한 모듈
 from email.mime.multipart import MIMEMultipart  # 메일의 Data 영역의 메시지를 만드는 모듈
 from email.mime.text import MIMEText  # 메일의 본문 내용을 만드는 모듈
 from email.mime.image import MIMEImage  # 메일의 이미지 파일을 base64 형식으로 변환하기 위한 모듈
+
+# smpt 서버와 연결
+gmail_smtp = "smtp.gmail.com"  # gmail smtp 주소
+gmail_port = 465  # gmail smtp 포트번호. 고정(변경 불가)
+smtp = smtplib.SMTP_SSL(gmail_smtp, gmail_port)
  
+# 로그인
+my_account = os.environ.get('email_account')  # .env 파일에 저장한 이메일
+my_password = os.environ.get('email_password')  # .env 파일에 저장한 App Password
+print(f'smtp login: ', my_account)
+# print(my_account, my_password)
+smtp.login(my_account, my_password)
 
 def sendEmail(addr: str, title: str, message: str):
     # 메일을 받을 계정
@@ -38,20 +49,6 @@ def sendEmail(addr: str, title: str, message: str):
         print("정상적으로 메일이 발송되었습니다.")
     else:
         print("받으실 메일 주소를 정확히 입력하십시오.")
-
-    
- 
-# smpt 서버와 연결
-gmail_smtp = "smtp.gmail.com"  # gmail smtp 주소
-gmail_port = 465  # gmail smtp 포트번호. 고정(변경 불가)
-smtp = smtplib.SMTP_SSL(gmail_smtp, gmail_port)
- 
-# 로그인
-my_account = os.environ.get('email_account')
-my_password = os.environ.get('email_password')
-print(f'smtp login: ', my_account)
-# print(my_account, my_password)
-smtp.login(my_account, my_password)
  
 
 def quit():
